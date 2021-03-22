6c1e840eafa17bdb7bfe9d7089d87a1b6d8a578a
Suraj Shrestha <syuraj@gmail.com>
2021-03-21 23:29:39 -0400

    wip - copying android folder from nepaltoday app

--- app/App.js
+++ app/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { persistor, store } from './store';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -7,8 +7,14 @@ import theme from './theme';
 import { Container, Root, StyleProvider } from 'native-base';
 import { StatusBar } from 'react-native';
 import { RootNavigator } from './navigation/RootNavigator';
+import RNBootSplash from 'react-native-bootsplash';
 
 const App = () => {
+  useEffect(() => {
+    console.log('reached here');
+    RNBootSplash.hide().catch(err => console.log(err));
+  });
+
   return (
     <Root>
       <Provider store={store}>
--- index.js
+++ index.js
@@ -5,4 +5,6 @@ import { AppRegistry } from 'react-native';
 import App from './app/App';
 import { name as appName } from './app.json';
 
+console.log('inside here');
+
 AppRegistry.registerComponent(appName, () => App);
